9a500ebe61e0ddd2ff006d411074a7fef83c7058
danielfalcaovt <danielfalcaoviktorio@gmail.com>
2024-02-12 08:41:21 -0300

    done project

--- index.js
+++ index.js
@@ -2,18 +2,16 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+import express from "express";
+import path from "path";
+import fs from "fs";
+import cors from "cors";
+const __dirname = import.meta.dirname;
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
+const app = express();
+app.use(express.static('public'));
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -24,9 +22,31 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/*",(req,res)=>{
+  const url = req.params;
+  if (url[0].length > 10){
+    const futureDate = new Date();
+    futureDate.setTime(url[0]);
+    console.log(futureDate.toUTCString());
+    res.json({"unix":Date.parse(futureDate),'utc':futureDate.toUTCString()});
+  }else{
+    console.log('here');
+    const options = {
+      weekday:'short',
+      year:'numeric',
+      month:'long',
+      day:'numeric'
+    };
+    console.log(url[0]);
+    const data = new Date(`${url[0]}`).toUTCString();
+    res.json({
+      'unix':Date.parse(url[0]),
+      "utc":data
+    });
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + 3000); 
 });
